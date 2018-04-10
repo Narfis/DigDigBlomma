1b219ad723a72b29ba78def728baa3abc3a60fa7
vendela01 <38098976+vendela01@users.noreply.github.com>
2018-04-10 14:24:32 +0200

    HealtBar

---
+++ DigDigBlomma/DigDigBlomma/Healthbar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigDigBlomma
+{
+    class Healthbar
+    {
+        Texture2D healthTexture;
+        Rectangle healthRectangle;
+
+        public Healthbar()
+        {
+            healthTexture = Content.Load<Texture2D>("health");
+            healthRectangle = new Rectangle(50, 20, player.health, 20);
+        }
+    }
+}
